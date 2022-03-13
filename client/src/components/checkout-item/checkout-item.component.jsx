b21beb0b44a7eb4bff92159df05b8f4fd562ecce
ormanor1 <ormanor@gmail.com>
2022-03-13 20:32:38 +0200

    add server and stripe functions

---
+++ client/src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from '../../redux/cart/cart.actions';
+
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  TextContainer,
+  QuantityContainer,
+  RemoveButtonContainer,
+} from './checkout-item.styles';
+
+const CheckoutItem = ({ cartItem }) => {
+  const dispatch = useDispatch();
+  const clearItem = (item) => dispatch(clearItemFromCart(item));
+  const addItem = (item) => dispatch(addItem(item));
+  const removeItem = (item) => dispatch(removeItem(item));
+
+  const { name, imageUrl, price, quantity } = cartItem;
+
+  return (
+    <CheckoutItemContainer>
+      <ImageContainer>
+        <img src={imageUrl} alt='item' />
+      </ImageContainer>
+      <TextContainer>{name}</TextContainer>
+      <QuantityContainer>
+        <div onClick={() => removeItem(cartItem)}>&#10094;</div>
+        <span>{quantity}</span>
+        <div onClick={() => addItem(cartItem)}>&#10095;</div>
+      </QuantityContainer>
+      <TextContainer>{price}</TextContainer>
+      <RemoveButtonContainer onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </RemoveButtonContainer>
+    </CheckoutItemContainer>
+  );
+};
+
+export default CheckoutItem;
---
+++ client/src/pages/shop/shop.component.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
+
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+
+const ShopPage = ({ match }) => {
+  const dispatch = useDispatch();
+  const fetchCollectionsStartHandler = () => dispatch(fetchCollectionsStart());
+
+  useEffect(() => {
+    fetchCollectionsStartHandler();
+  }, []);
+
+  return (
+    <div className='shop-page'>
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
+};
+
+export default ShopPage;
